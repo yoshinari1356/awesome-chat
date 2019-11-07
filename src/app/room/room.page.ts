90f4a9d6662f7b06ad6a32291c37d77f1c8df2a4
二瓶由成 <n0802jp2@gmail.com>
2019-11-07 20:25:32 +0900

    Implemented room

--- src/app/room/room.page.ts
+++ src/app/room/room.page.ts
@@ -8,17 +8,29 @@ import * as firebase from 'firebase';
   styleUrls: ['./room.page.scss'],
 })
 export class RoomPage implements OnInit {
+  rooms = [];
 
   constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        firebase.database().ref('chatrooms/').on('value', resp => {
+          if (resp) {
+            this.rooms = [];
+            resp.forEach(childSnapshot => {
+              const room = childSnapshot.val();
+              room.key = childSnapshot.key;
+              this.rooms.push(room);
+            });
+          }
+        });
       } else {
         this.navCtrl.navigateRoot('signin');
       }
     });
   }
+
   async signOut() {
     try {
       await firebase.auth().signOut();
@@ -26,6 +38,8 @@ export class RoomPage implements OnInit {
 
     } catch (error) {}
   }
-
+  joinRoom(key) {
+    this.navCtrl.navigateRoot('chat/' + key);
+  }
 
 }
