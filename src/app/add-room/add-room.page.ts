f88d4bb6150d792f9b61e5b537d7f07cfd7bb155
二瓶由成 <n0802jp2@gmail.com>
2019-11-07 20:31:47 +0900

    Implemented add-room

--- src/app/add-room/add-room.page.ts
+++ src/app/add-room/add-room.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import * as firebase from 'Firebase';
 
 @Component({
   selector: 'app-add-room',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-room.page.scss'],
 })
 export class AddRoomPage implements OnInit {
+  data: { roomname: string } = { roomname: '' };
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  addRoom() {
+    console.log("addRoom");
+    const newData = firebase.database().ref('chatrooms/')
+    .push({
+      roomname: this.data.roomname
+    });
+    this.navCtrl.navigateRoot('room');
+  }
 }
