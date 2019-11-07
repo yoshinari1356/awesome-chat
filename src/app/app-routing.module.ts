e23359b9be01d8351b02336780fd256a14789ceb
二瓶由成 <n0802jp2@gmail.com>
2019-11-07 20:08:08 +0900

    create signin & room page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'room', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' },
+  { path: 'room', loadChildren: './room/room.module#RoomPageModule' },
 ];
+
 
 @NgModule({
   imports: [
